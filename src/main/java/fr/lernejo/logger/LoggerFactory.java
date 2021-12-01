2a8fbddac8359d12da52019de0395325e4f12eb4
maxleclerc2 <mleclerc@et.esiea.fr>
2021-12-01 13:12:28 +0100

    Added CompositeLogger

--- src/main/java/fr/lernejo/logger/LoggerFactory.java
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -3,6 +3,12 @@ package fr.lernejo.logger;
 public class LoggerFactory {
 
     public static Logger getLogger(String name) {
-        return new ContextualLogger(name, new FileLogger("./logs.txt"));
+        return new ContextualLogger(
+            name,
+            new CompositeLogger(
+                new ConsoleLogger(),
+                new FileLogger("./logs.txt")
+            )
+        );
     }
 }
