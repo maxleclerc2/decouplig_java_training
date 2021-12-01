462a4a8abff0eaa29e65b525259dde0afa256565
maxleclerc2 <mleclerc@et.esiea.fr>
2021-12-01 13:06:25 +0100

    Added ContextualLogger and FileLogger

--- src/main/java/fr/lernejo/guessgame/Launcher.java
+++ src/main/java/fr/lernejo/guessgame/Launcher.java
@@ -1,8 +1,12 @@
 package fr.lernejo.guessgame;
 
+import fr.lernejo.logger.Logger;
+import fr.lernejo.logger.LoggerFactory;
+
 import java.security.SecureRandom;
 
 public class Launcher {
+    private static final Logger logger = LoggerFactory.getLogger("launcher");
 
     public static void main(String[] args) {
         try {
@@ -14,6 +18,7 @@ public class Launcher {
                 long randomNumber = random.nextInt(100); // génère un nombre entre 0 (inclus) et 100 (exclus)
 
                 game.initialize(randomNumber, Long.MAX_VALUE);
+                logger.log("Initialized game with HUMAN");
                 game.loopUntilPlayerSucceed();
             } else if (args[0].equals("-auto")) {
                 Player player = new ComputerPlayer();
@@ -23,9 +28,11 @@ public class Launcher {
                 long randomNumber = random.nextInt(100); // génère un nombre entre 0 (inclus) et 100 (exclus)
 
                 game.initialize(randomNumber, Long.parseLong(args[1]));
+                logger.log("Initialized game with MYSELF");
                 game.loopUntilPlayerSucceed();
             }
         } catch (IndexOutOfBoundsException e) {
+            logger.log("Wrong args");
             System.out.println("Usage:");
             System.out.println("-interactive    : lets the player play the game");
             System.out.println("-auto <max>     : lets the computer play the game with <max> being the maximum tries");
