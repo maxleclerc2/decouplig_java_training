b2f4e2b4bbdcf352b197d3a6b70e6bbe3b5b7279
maxleclerc2 <mleclerc@et.esiea.fr>
2021-12-01 11:20:10 +0100

    Added ComputerPlayer

--- src/main/java/fr/lernejo/guessgame/Launcher.java
+++ src/main/java/fr/lernejo/guessgame/Launcher.java
@@ -5,13 +5,30 @@ import java.security.SecureRandom;
 public class Launcher {
 
     public static void main(String[] args) {
-        Player player = new HumanPlayer();
-        Simulation game = new Simulation(player);
+        try {
+            if (args[0].equals("-interactive")) {
+                Player player = new HumanPlayer();
+                Simulation game = new Simulation(player);
 
-        SecureRandom random = new SecureRandom();
-        long randomNumber = random.nextInt(100); // génère un nombre entre 0 (inclus) et 100 (exclus)
+                SecureRandom random = new SecureRandom();
+                long randomNumber = random.nextInt(100); // génère un nombre entre 0 (inclus) et 100 (exclus)
 
-        game.initialize(randomNumber);
-        game.loopUntilPlayerSucceed();
+                game.initialize(randomNumber, Long.MAX_VALUE);
+                game.loopUntilPlayerSucceed();
+            } else if (args[0].equals("-auto")) {
+                Player player = new ComputerPlayer();
+                Simulation game = new Simulation(player);
+
+                SecureRandom random = new SecureRandom();
+                long randomNumber = random.nextInt(100); // génère un nombre entre 0 (inclus) et 100 (exclus)
+
+                game.initialize(randomNumber, Long.parseLong(args[1]));
+                game.loopUntilPlayerSucceed();
+            }
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Usage:");
+            System.out.println("-interactive    : lets the player play the game");
+            System.out.println("-auto <max>     : lets the computer play the game with <max> being the maximum tries");
+        }
     }
 }
